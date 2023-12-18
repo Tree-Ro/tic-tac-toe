57f4e049e410e8441276bdaa6441cbec4011a903
David Steiner <david1.steiner@gmail.com>
2023-12-18 16:54:16 +0100

    Created overall module layout for the game in js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('hello');
+const ticTacToe = {
+    gameBoard: {
+        row1: ['p', 'p', 'p'],
+        row2: ['p', 'p', 'p'],
+        row3: ['p', 'p', 'p'],
+    },
+
+    init: function () {
+        this.resetBoard();
+    },
+
+    cacheDom: function () {},
+
+    bindEvents: function () {},
+
+    render: function () {},
+
+    CreatePlayer: function (name, marker) {
+        this.name = name;
+        this.marker = marker.toUpperCase();
+    },
+
+    resetBoard: function () {
+        this.gameBoard = {
+            row1: ['p', 'p', 'p'],
+            row2: ['p', 'p', 'p'],
+            row3: ['p', 'p', 'p'],
+        };
+    },
+};
+
+ticTacToe.init();
